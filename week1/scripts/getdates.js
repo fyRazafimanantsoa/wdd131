fbae97d6bf19f88fce09db78f31f68b15dcdb3a9
FyRazafimanantsoa <fyratsoa@gmail.com>
2025-01-12 08:01:23 +0300

    homepage

---
+++ week1/scripts/getdates.js
@@ -0,0 +1,9 @@
+// Get current year
+const currentYearSpan = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+currentYearSpan.textContent = currentYear;
+
+// Get last modified date
+const lastModified = document.lastModified;
+const lastModifiedParagraph = document.getElementById("lastModified");
+lastModifiedParagraph.textContent = `Last Modified: ${lastModified}`;
