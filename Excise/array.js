dfa13d370ea213481d6fc7687703f0d51805169d
FyRazafimanantsoa <fyratsoa@gmail.com>
2025-02-09 00:48:11 +0300

    filtered

---
+++ Excise/array.js
@@ -0,0 +1,7 @@
+//  arrays.js
+const steps = ["one", "two", "three"];
+const listTemplate(step)  => { steps.map(steps)
+  return //the html string made from step
+}
+const stepsHtml = listTemplate; // use map to convert the list from strings to HTML
+document.querySelector("#myList").innerHTML = stepsHtml;// set the innerHTML
